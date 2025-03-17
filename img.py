09a767eb63b7fdacdae20582cbc3c619b54fbcf7
hirokide314 <h.asada.ide@gmail.com>
2025-03-18 05:33:27 +0900

    test

---
+++ img.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import networkx as nx
+import matplotlib.pyplot as plt
+
+st.title("IMAGE")
+G = nx.DiGraph()
+G.add_node("あ")
+G.add_node("い")
+G.add_node("う")
+G.add_node("え")
+G.add_node("お")
+G.add_node("か")
+G.add_node("き")
+G.add_edge("あ","い")
+G.add_edge("え","い")
+G.add_edge("あ","う")
+G.add_edge("う","お")
+G.add_edge("う","え")
+G.add_edge("う","か")
+G.add_edge("か","き")
+
+
+pr = nx.pagerank(G)
+plt.rcParams["font.family"] = 'MS Gothic'
+# plt.figure(figsize=(10,10))
+pos = nx.spring_layout(G,k=0.3)
+nx.draw_networkx_edges(G, pos=pos)
+nx.draw_networkx_nodes(G, pos=pos, node_color=list(pr.values()), cmap=plt.cm.Reds, node_size=[5000*v for v in pr.values()], label=list(G.nodes))
+nx.draw_networkx_labels(G, pos=pos, font_family='MS Gothic')
+
+
+# nx.draw(G, with_labels=True, font_family="MS Gothic") 
+plt.savefig("./graph.png")
+
+st.image("./graph.png")
